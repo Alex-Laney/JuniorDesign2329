e7a7946d46077f7687001265dcf6e3c2390dda44
Jacob Singer <jsinger32@gatech.edu>
2023-01-30 10:50:22 -0500

    All Screens Have Menu Bar
    The four main screens (Composers, Terms, Works, Quizzes) all have the same menu bar and circular spiral menu selector that main has been given.

--- lib/Quizzes/Quizzes.dart
+++ lib/Quizzes/Quizzes.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:circular_menu/circular_menu.dart';
+import 'dart:math' as math;
 
 class QuizzesScreen extends StatelessWidget {
   const QuizzesScreen({super.key});
@@ -6,7 +8,48 @@ class QuizzesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ButtonStyle style =
-    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));;
+    final int iconColor = 300;
+
+    // returns the circular spanning button for module selection
+    CircularMenu buildCircularDialMainMenu() {
+      return CircularMenu(
+        toggleButtonBoxShadow: const [],
+        startingAngleInRadian: 7 * math.pi / 6,
+        endingAngleInRadian: 11 * math.pi / 6,
+        items: [
+          CircularMenuItem(
+            boxShadow: const [],
+            color: Colors.yellow[iconColor],
+            icon: Icons.person,
+            iconColor: Colors.black,
+            onTap: () => Navigator.pushNamed(context, '/composers'),
+          ),
+          CircularMenuItem(
+            boxShadow: const [],
+            color: Colors.blue[iconColor],
+            icon: Icons.music_note,
+            iconColor: Colors.black,
+            onTap: () => Navigator.pushNamed(context, '/works'),
+          ),
+          CircularMenuItem(
+            boxShadow: const [],
+            color: Colors.green[iconColor],
+            icon: Icons.quiz,
+            iconColor: Colors.black,
+            onTap: () => Navigator.pushNamed(context, '/quizzes'),
+          ),
+          CircularMenuItem(
+            boxShadow: const [],
+            color: Colors.red[iconColor],
+            icon: Icons.book,
+            iconColor: Colors.black,
+            onTap: () => Navigator.pushNamed(context, '/terms'),
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Quizzes'),
@@ -22,6 +65,26 @@ class QuizzesScreen extends StatelessWidget {
               ),
             ],
           ),
-        ));
+        ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: buildCircularDialMainMenu(),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () => Navigator.pushNamed(context, '/'),
+              tooltip: 'Home',
+              icon: const Icon(Icons.home, color: Colors.black45),
+            ),
+            IconButton(
+              onPressed: () {},
+              tooltip: 'Settings',
+              icon: const Icon(Icons.settings, color: Colors.black45),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
