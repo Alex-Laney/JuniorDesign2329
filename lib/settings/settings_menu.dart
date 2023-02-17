6ffe14d834bca4cdc0c546c8418c65abb151bd29
SpeakerRegional <marksman1500@gmail.com>
2023-02-16 23:01:09 -0500

    Add settings screen backbone

---
+++ lib/settings/settings_menu.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:artifact/circular_dial_menu.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: CircularDialMenu.build(context),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () => Navigator.pushNamedAndRemoveUntil(
+                  context, '/', (route) => false),
+              tooltip: 'Home',
+              icon: const Icon(Icons.home, color: Colors.black45),
+            ),
+            IconButton(
+              onPressed: () {},
+              tooltip: 'Settings',
+              icon: const Icon(Icons.settings, color: Colors.black45),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
