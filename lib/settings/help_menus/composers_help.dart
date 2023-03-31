4ff367f0ac41b00b7ccd66b9a941a06a40ad57ba
Andrew Chen <achen379@gatech.edu>
2023-03-31 02:03:27 -0400

    Added help screen for composers module

--- lib/settings/help_menus/composers_help.dart
+++ lib/settings/help_menus/composers_help.dart
@@ -1,4 +1,7 @@
+import 'package:circular_menu/circular_menu.dart';
 import 'package:flutter/material.dart';
+
+import '../../circular_dial_menu.dart';
 
 class ComposersHelpScreen extends StatelessWidget {
   const ComposersHelpScreen({super.key});
@@ -13,10 +16,39 @@ class ComposersHelpScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            RichText(
+            const Text('Composers Help', style: TextStyle(fontSize: 40)),
+        Padding(
+          padding: const EdgeInsets.all(40),
+            child: RichText(
+              textAlign: TextAlign.justify,
               text: const TextSpan(
-                text: '',
+                text: 'The composers page contains biographies about various famous composers who have influenced '
+                    'the development of classical music. Each page covers the upbringing and career of a composer and gives'
+                    ' readers insight on how they developed as a musician.',
+                style:
+                  const TextStyle(fontSize: 20, color: Colors.black),
+                ),
               ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: CircularDialMenu.build(context),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () => Navigator.pushNamedAndRemoveUntil(
+                  context, '/', (route) => false),
+              tooltip: 'Home',
+              icon: const Icon(Icons.home, color: Colors.black45),
+            ),
+            IconButton(
+              onPressed: () {},
+              tooltip: 'Settings',
+              icon: const Icon(Icons.settings, color: Colors.black45),
             ),
           ],
         ),
