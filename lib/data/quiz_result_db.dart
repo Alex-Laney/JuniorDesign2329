6a314d3a015b5250739ec2a8c0571aedb65e57c8
SpeakerRegional <marksman1500@gmail.com>
2023-04-01 10:52:30 -0400

    Add placeholder quiz results data

--- lib/data/quiz_result_db.dart
+++ lib/data/quiz_result_db.dart
@@ -2,14 +2,22 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 class QuizResultDatabase {
   // TODO: make the database for the leaderboard
-  //  General idea: each entry is another list for each quiz;
+  //  General idea: each entry is top score with name;
   //  Store the top 5 results
   List leaderboardList = [];
 
   final _userBox = Hive.box('userBox');
 
   /// Run this method if 1st time running the app
-  void createInitialData() {}
+  void createInitialData() {
+    leaderboardList = [
+      ["Name 1", 0],
+      ["Name 2", 0],
+      ["Name 3", 0],
+      ["Name 4", 0],
+      ["Name 5", 0]
+    ];
+  }
 
   /// Load data from result database.
   ///
