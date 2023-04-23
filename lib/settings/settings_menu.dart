cc3bc4add07d433e4deca5eee40fd22aaa880a4a
SpeakerRegional <marksman1500@gmail.com>
2023-04-23 01:05:45 -0400

    Add reset data button

--- lib/settings/settings_menu.dart
+++ lib/settings/settings_menu.dart
@@ -3,6 +3,7 @@ import 'package:artifact/bottom_navigation_bar/bottom_button_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:artifact/bottom_navigation_bar/circular_dial_menu.dart';
 import 'package:artifact/music_box.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -36,27 +37,76 @@ class _SettingsScreenState extends State<SettingsScreen> {
               style: OutlinedButton.styleFrom(
                 backgroundColor: Color.fromRGBO(255, 255, 255, 1.0),
                 foregroundColor: Color.fromRGBO(0, 0, 0, 1.0),
-                side: BorderSide(width: 5.0, color: Color.fromRGBO(232, 163, 99, 1.0)),
+                side: BorderSide(
+                    width: 5.0, color: Color.fromRGBO(232, 163, 99, 1.0)),
                 elevation: 5,
                 //fixedSize: Size:,
               ),
               onPressed: () {
                 Navigator.pushNamed(context, '/about');
               },
-              child: Text('About', style: TextStyle(fontSize: 80, color: Colors.black)),
+              child: Text('About',
+                  style: TextStyle(fontSize: 80, color: Colors.black)),
             ),
             OutlinedButton(
               style: OutlinedButton.styleFrom(
                 backgroundColor: Color.fromRGBO(255, 255, 255, 1.0),
                 foregroundColor: Color.fromRGBO(0, 0, 0, 1.0),
-                side: BorderSide(width: 5.0, color: Color.fromRGBO(232, 163, 99, 1.0)),
+                side: BorderSide(
+                    width: 5.0, color: Color.fromRGBO(232, 163, 99, 1.0)),
                 elevation: 5,
                 //fixedSize: Size:,
               ),
               onPressed: () {
                 Navigator.pushNamed(context, '/help');
               },
-              child: Text('Help', style: TextStyle(fontSize: 80, color: Colors.black)),
+              child: Text('Help',
+                  style: TextStyle(fontSize: 80, color: Colors.black)),
+            ),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Color.fromRGBO(255, 255, 255, 1.0),
+                foregroundColor: Color.fromRGBO(0, 0, 0, 1.0),
+                side: BorderSide(
+                    width: 5.0, color: Color.fromRGBO(232, 163, 99, 1.0)),
+                elevation: 5,
+                //fixedSize: Size:,
+              ),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    scrollable: true,
+                    title: Text('Reset Data'),
+                    content: Text(
+                      'Are you sure you want to reset your data? You will lose'
+                      ' all of your points and rewards. This is not reversible.',
+                    ),
+                    actions: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                        ),
+                        onPressed: () {
+                          Hive.box('userBox').clear();
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Reset'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Cancel'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: Text(
+                'Reset Data',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
             ),
             IconButton(
               onPressed: () => {
@@ -71,7 +121,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
               icon: mute
                   ? Icon(Icons.volume_up, color: Colors.black45)
                   : Icon(Icons.volume_off, color: Colors.black45),
-            ),  
+            ),
           ],
         ),
       ),
